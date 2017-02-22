2d5193d5390e825d743b262548ae3a406195435d
Laura Suttle <lsuttle@localhost.localdomain>
2017-02-21 20:21:22 -0500

    Added percentage update

--- bokehapps/bokeh-sliders.py
+++ bokehapps/bokeh-sliders.py
@@ -2,7 +2,7 @@ from bokeh.io import curdoc
 
 import pandas as pd
 from bokeh.charts import Scatter, output_file, show
-from bokeh.models.widgets import Dropdown
+from bokeh.models.widgets import Dropdown, Div
 from bokeh.layouts import row, widgetbox, layout
 from bokeh.models import Select
 from bokeh.models import ColumnDataSource
@@ -13,7 +13,7 @@ import sqlite3
 
 sql = """
 SELECT _projectid, year, total_price_including_optional_support,
-school_state, total_donations, perc_funded, primary_focus_subject
+school_state, total_donations, perc_funded, primary_focus_subject, completed
 FROM projects
 """
 
@@ -23,14 +23,19 @@ projects = pd.read_sql(sql, con)
 df = projects[(projects.year == 2015) & (projects.total_price_including_optional_support < 1000)]
 
 def make_plot(df):
-    p = figure(title="markers.py example", webgl=True)
+    prob = get_prob(source.data['completed'])
+    p = figure(title='{}% funding chance'.format(prob), webgl=True)
     p.scatter(x='total_price_including_optional_support',
               y='perc_funded',
               source = df,
               size = 10)
     return p
 
-def get_dataset(src, state, field):
+def get_prob(values):
+    #return int(sum(values)/len(values) * 100)
+    return int(float(sum(values))/len(values) * 100)
+
+def get_dataset(src, state, field, sample):
     l = [state, field]
     fd = {}
     if state is not 'All':
@@ -40,19 +45,25 @@ def get_dataset(src, state, field):
     df = src.copy()
     for key, values in fd.items():
         df = df[df[key].isin(values)]
-    return ColumnDataSource(data=df)
+    return ColumnDataSource(data=df.sample(sample))
 
 def update_plot(attrname, old, new):
     state = stateselect.value
     field = fieldselect.value
-    src = get_dataset(df, state, field)
+    src = get_dataset(df, state, field, 100)
     source.data.update(src.data)
+    prob = get_prob(source.data['completed'])
+    p.title.text = '{}% funding chance'.format(prob)
+    div.text = '{}% funding chance'.format(prob)
 
 states = ['All'] + sorted(list(df.school_state.unique()))
 fields = ['All'] + sorted(list(df.primary_focus_subject.unique()))
 
 # get data
-source = get_dataset(df, states[0], fields[0])
+source = get_dataset(df, states[0], fields[0], 100)
+
+# base prob
+prob = get_prob(source.data['completed'])
 
 # create figure
 p = make_plot(source)
@@ -60,8 +71,9 @@ p = make_plot(source)
 # create widgets
 stateselect = Select(title='Select State', value=states[0], options=states)
 fieldselect = Select(title='Select Focus Subject', value=fields[0], options=fields)
+div = Div(text='{}% funding chance'.format(prob), width=200, height=100)
 
-w = widgetbox(stateselect, fieldselect)
+w = widgetbox(stateselect, fieldselect, div)
 
 # filter settings
 stateselect.on_change('value', update_plot)

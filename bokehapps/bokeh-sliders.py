e21c19fee7cebfaaa7ba2159327ca80e92edc354
Laura Suttle <lsuttle@localhost.localdomain>
2017-02-26 19:31:42 -0500

    Adding template

--- bokehapps/bokeh-sliders.py
+++ bokehapps/bokeh-sliders.py
@@ -1,11 +1,11 @@
 from bokeh.io import curdoc
 
 import pandas as pd
-from bokeh.charts import Scatter, output_file, show
+from bokeh.charts import Scatter#, output_file, show
 from bokeh.models.widgets import Dropdown, Div
 from bokeh.layouts import row, widgetbox, layout
-from bokeh.models import Select
-from bokeh.models import ColumnDataSource
+from bokeh.models import Select, ColumnDataSource
+
 
 from bokeh.plotting import figure
 import sqlite3
@@ -33,19 +33,27 @@ def make_plot(df):
 
 def get_prob(values):
     #return int(sum(values)/len(values) * 100)
+    if len(values) == 0:
+        return 0
     return int(float(sum(values))/len(values) * 100)
 
+def return_sample(data, sample):
+    if len(data) < sample:
+        return data
+    return data.sample(sample)
+
 def get_dataset(src, state, field, sample):
-    l = [state, field]
-    fd = {}
-    if state is not 'All':
-        fd['school_state'] = [state]
-    if field is not 'All':
-        fd['primary_focus_subject'] = [field]
     df = src.copy()
-    for key, values in fd.items():
-        df = df[df[key].isin(values)]
-    return ColumnDataSource(data=df.sample(sample))
+    if state == field:
+        return ColumnDataSource(data=return_sample(df, sample))
+    if state is not 'All':
+        df = df[df['school_state'].isin([state])]
+        return ColumnDataSource(data=return_sample(df, sample))
+    if field is not 'All':
+        df = df[df['primary_focus_subject'].isin([field])]
+        return ColumnDataSource(data=return_sample(df, sample))
+
+
 
 def update_plot(attrname, old, new):
     state = stateselect.value
@@ -55,6 +63,8 @@ def update_plot(attrname, old, new):
     prob = get_prob(source.data['completed'])
     p.title.text = '{}% funding chance'.format(prob)
     div.text = '{}% funding chance'.format(prob)
+    div2.text = '{}'.format(state)
+    div3.text = '{}'.format(field)
 
 states = ['All'] + sorted(list(df.school_state.unique()))
 fields = ['All'] + sorted(list(df.primary_focus_subject.unique()))
@@ -72,13 +82,18 @@ p = make_plot(source)
 stateselect = Select(title='Select State', value=states[0], options=states)
 fieldselect = Select(title='Select Focus Subject', value=fields[0], options=fields)
 div = Div(text='{}% funding chance'.format(prob), width=200, height=100)
+div2 = Div(text='{}'.format(stateselect.value), width=200, height=100)
+div3 = Div(text='{}'.format(fieldselect.value), width=200, height=100)
 
-w = widgetbox(stateselect, fieldselect, div)
+
+w = widgetbox(stateselect, fieldselect, div, div2, div3)
 
 # filter settings
-stateselect.on_change('value', update_plot)
-fieldselect.on_change('value', update_plot)
+#stateselect.on_change('value', update_plot)
+#fieldselect.on_change('value', update_plot)
 
+for widget in [stateselect, fieldselect]:
+    widget.on_change('value', update_plot)
 
 # layout
 l = layout([[w, p]])
--- flaskapps/hello.py
+++ flaskapps/hello.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     script=autoload_server(model=None,app_path="/bokeh-sliders",url="http://10.140.0.46/bokehapp")
-    return render_template('hello.html',bokS=script)
+    #return render_template('hello.html',bokS=script)
+    return render_template('index.html',bokS=script)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
